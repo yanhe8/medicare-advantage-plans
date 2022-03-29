559286054ed5f22262d88bce7aaa0a1881fdf5fe
Yan_PWBM <yanhe8@wharton.upenn.edu>
2022-03-29 14:42:49 -0400

    deploy the app (update)

--- shinyApp/deployment.R
+++ shinyApp/deployment.R
@@ -4,7 +4,7 @@ rsconnect::setAccountInfo(name='yanheupenn',
                           token='C31C823B0CB197134F1228D8E69507E4',
                           secret='2qlbFM/NQkJ2zDYcWUcG2xb87/0JosNXqG/OyNDn')
 deployApp(account = "yanheupenn", appName = 'MA_plans')
-rsconnect::deployApp()
+# rsconnect::deployApp()
 
 # library(profvis)
 # profvis(runApp())
